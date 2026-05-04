fdd78a1737e15a9bc674ca89411dc95257f98402
CamdenBohanan <165093553+CamdenBohanan@users.noreply.github.com>
2026-05-03 23:30:01 -0400

    Delete Downloads directory

--- Downloads/ASE_456_Work/ASE456_Individual_Project_Squire copy/squire/android/app/src/main/kotlin/com/example/manga_reader_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.manga_reader_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Downloads/Squire/squire/android/app/src/main/kotlin/com/example/squire/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.squire
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
